fe83319effb64d4780f0fd776bc728f389ff9ca9
pedrothehood <peter.gloor2@bluewin.ch>
2026-06-06 07:53:14 +0200

    Boardtypes konfiguriert ESP32_S3_SUPERMINI

--- pins_config.h
+++ pins_config.h
@@ -1,38 +1,69 @@
 // Pins Boardübergreifend definieren
 #pragma once
 
+// --- Board-Auswahl ---
+// nur ein Profil aktivieren !!!!!
+//#define YD_ESP32_S3
+#define ESP32_S3_SUPERMINI
+
+// ==========================================
+// KONFIGURATION DER PINS JE NACH BOARD
+// ==========================================
+#if defined(YD_ESP32_S3)
+
+  const int SENSOR_RX = 16;
+  const int SENSOR_TX = 17;
+  const int CONFIG_BUTTON_PIN = 7;  // D5 oder GPIO7
+  // Erlaubte Werte: INPUT (für externen Widerstand) oder INPUT_PULLUP (für internen Widerstand)
+  #define CONFIG_BUTTON_MODE INPUT  // MIT externem 10kOhm Widerstand!
+  const int LED_BLINK_PIN = 6;
+
+#elif defined(ESP32_S3_SUPERMINI)
+
+  const int SENSOR_RX = 5;
+  const int SENSOR_TX = 4;
+  const int CONFIG_BUTTON_PIN = 1;  // D5 oder GPIO7
+  // Erlaubte Werte: INPUT (für externen Widerstand) oder INPUT_PULLUP (für internen Widerstand)
+  #define CONFIG_BUTTON_MODE INPUT_PULLUP  // OHNE externem 10kOhm Widerstand!
+  const int LED_BLINK_PIN = 2;
+
+#else
+  #error "Bitte definiere zuerst ein gültiges Board-Profil!"
+#endif
+
+
 // ==========================================
 // VARIANTE 1: YD-ESP32-S3 (Wird als ESP32S3 Dev Module kompiliert)
 // ==========================================
 #if defined(CONFIG_IDF_TARGET_ESP32S3)
-  #define BOARD_TYPE "YD-ESP32-S3"
-  
-  const int  SENSOR_RX = 16;
-  const int  SENSOR_TX = 17;
-  const int  CONFIG_BUTTON_PIN = 7;  // D5 oder GPIO7
+#define BOARD_TYPE "YD-ESP32-S3"
 
-  // DIREKTE MODUS-KONFIGURATION
+//const int SENSOR_RX = 16;
+//const int SENSOR_TX = 17;
+//const int CONFIG_BUTTON_PIN = 7;  // D5 oder GPIO7
+
+// DIREKTE MODUS-KONFIGURATION
 // Erlaubte Werte: INPUT (für externen Widerstand) oder INPUT_PULLUP (für internen Widerstand)
-#define CONFIG_BUTTON_MODE INPUT
+//#define CONFIG_BUTTON_MODE INPUT
 
-  const int  LED_BLINK_PIN = 6;
+//const int LED_BLINK_PIN = 6;
 
-  /* const int PIN_STATUS_LED = 2;       // Blaue LED auf der Rückseite
+/* const int PIN_STATUS_LED = 2;       // Blaue LED auf der Rückseite
   const int PIN_USER_BUTTON = 0;      // Boot-Knopf
   const int PIN_I2C_SDA     = 4;      // Beispiel: Ihre I2C-Belegung für S3
   const int PIN_I2C_SCL     = 5;
   const int PIN_ADC_BATTERY = 1;      // Analoger Pin für Batterie-Messung  */
- 
-  // ==========================================
+
+// ==========================================
 // VARIANTE 2: ESP32-C3 SuperMini (Sehr kompakter RISC-V)
 // ==========================================
 #elif defined(CONFIG_IDF_TARGET_ESP32C3)
-  #define BOARD_TYPE "ESP32-C3 SuperMini"
-  
+#define BOARD_TYPE "ESP32-C3 SuperMini"
+
 // ==========================================
 // VARIANTE 3: Klassischer ESP32 DevKit v1 (30 oder 38 Pins)
 // ==========================================
 #elif defined(CONFIG_IDF_TARGET_ESP32) && !defined(CONFIG_IDF_TARGET_ESP32S3) && !defined(CONFIG_IDF_TARGET_ESP32C3)
-  #define BOARD_TYPE "Classic ESP32 DevKit"
+#define BOARD_TYPE "Classic ESP32 DevKit"
 
 #endif
